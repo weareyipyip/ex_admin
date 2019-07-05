9003df7c4c1d081f5ed438284523d943dd675e8e
Ian Warshak <iwarshak@stripey.net>
2019-07-05 08:28:11 -0500

    more datetime fix

--- lib/ex_admin/utils.ex
+++ lib/ex_admin/utils.ex
@@ -297,20 +297,11 @@ defmodule ExAdmin.Utils do
   def confirm_message, do: gettext("Are you sure you want to delete this?")
 
   @doc false
-  # def to_datetime(%Ecto.DateTime{} = dt) do
-  #   {:ok, {date, {h, m, s, _ms}}} = Ecto.DateTime.dump(dt)
-  #   {date, {h, m, s}}
-  # end
   def to_datetime(%DateTime{} = dt) do
     %{month: month, day: day, year: year, hour: hour, minute: minute, second: second} = dt
     # {:ok, {date, {h, m, s, _ms}}} = DateTime.dump(dt)
-    {{month, day, year}, {hour, minute, second}}
+    {{year, month, day}, {hour, minute, second}}
   end
-
-  # def to_datetime(%DateTime{} = dt) do
-  #   DateTime.to_naive(dt)
-  #   |> NaiveDateTime.to_erl()
-  # end
 
   @doc false
   def format_time_difference({d, {h, m, s}}) do
